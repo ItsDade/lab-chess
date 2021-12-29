035483ad6a87fedbd835e918527feef8b74d2669
Davide <ferrari.dade@libero.it>
2021-12-29 10:59:13 +0100

    Azzerato tutto lol

--- lib/headers/Color.h
+++
@@ -1,7 +0,0 @@
-// Include guards
-#ifndef COLOR_H
-#define COLOR_H
-enum class Color {
-	White, Black, None
-};
-#endif // !COLOR_H
--- lib/sources/Game.cpp
+++
@@ -1,9 +0,0 @@
-#include "../headers/Game.h"
-
-#include <regex>
-
-void Game::nextMove(std::string move) {
-	if (std::regex_match(move, std::regex { "/[a - h][1 - 8][a - h][1 - 8]|xx xx/gm)" }))
-		return;
-
-}
